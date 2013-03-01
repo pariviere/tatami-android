21adaf0902d4c1be823b55437f71182ecf61a833
Pierre-Alain RIVIERE <pariviere@ippon.fr>
2013-03-01 22:36:49 +0100

    http://sandbox.tatamisoft.com is only for testing purpose. Revert to https://tatami.ippon.fr/

--- src/tatami/android/ClientURL.java
+++ src/tatami/android/ClientURL.java
@@ -1,10 +1,10 @@
 package tatami.android;
 
 public class ClientURL {
-	public static String ROOT		= "http://sandbox.tatamisoft.com/tatami/";
-	public static String AUTH		= "http://sandbox.tatamisoft.com/tatami/authentication";
-	public static String LOGIN		= "http://sandbox.tatamisoft.com/tatami/login";
-	public static String TIMELINE	= "http://sandbox.tatamisoft.com/tatami/rest/statuses/home_timeline";
-	public static String DETAILS	= "http://sandbox.tatamisoft.com/tatami/rest/statuses/details/%s";
-	public static String UPDATE		= "http://sandbox.tatamisoft.com/tatami/rest/statuses/update";
+	public static String ROOT		= "https://tatami.ippon.fr/tatami/";
+	public static String AUTH		= "https://tatami.ippon.fr/tatami/authentication";
+	public static String LOGIN		= "https://tatami.ippon.fr/tatami/login";
+	public static String TIMELINE	= "https://tatami.ippon.fr/tatami/rest/statuses/home_timeline";
+	public static String DETAILS	= "https://tatami.ippon.fr/tatami/rest/statuses/details/%s";
+	public static String UPDATE		= "https://tatami.ippon.fr/tatami/rest/statuses/update";
 }
